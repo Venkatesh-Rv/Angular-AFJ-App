81017c8ae5e75c0fd1f89c302d001ea929ec911a
Venkatesh-Rv <venkatrv34@gmail.com>
2021-07-20 13:30:48 +0530

    20/7/21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule,NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,13 +9,18 @@ import { FooterComponent } from './homepage/footer/footer.component';
 import { NecklaceComponent } from './product/necklace/necklace.component';
 import { CombosetsComponent } from './product/combosets/combosets.component';
 import { ShippingDetailsComponent } from './shipping-details/shipping-details.component';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductPageComponent } from './product/product-page/product-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
-// const routes: Routes = [
-//   { path: 'home', component:  },
-//   { path: 'prod', component: },
-// ]
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'neck', component: NecklaceComponent },
+  { path: 'comboset', component: CombosetsComponent },
+  { path: 'prod-page', component: ProductPageComponent },
+  { path: 'details', component: ShippingDetailsComponent },
+]
 
 @NgModule({
   declarations: [
@@ -25,13 +30,18 @@ import { Routes } from '@angular/router';
     FooterComponent,
     NecklaceComponent,
     CombosetsComponent,
-    ShippingDetailsComponent
+    ShippingDetailsComponent,
+    ProductPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes, { enableTracing: true }),
+    BrowserAnimationsModule,
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:[NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
--- src/app/homepage/homepage/homepage.component.ts
+++ src/app/homepage/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  //slider images
+
+  slides = [{ 'image': 'https://therichpost.com/wp-content/uploads/2021/02/Vuejs-Fashion-Ecommerce-Template-Free.png' },
+  { 'image': 'https://media.gettyimages.com/photos/jewelry-picture-id118199633?s=2048x2048' },
+  { 'image': 'https://therichpost.com/wp-content/uploads/2020/10/Angular-10-Learning-Education-Center-Free-Template-1.png' },
+  { 'image': 'https://therichpost.com/wp-content/uploads/2020/11/Reactjs-Easy-Shop-Free-Template-with-Source-Code.png' },
+  { 'image': 'https://therichpost.com/wp-content/uploads/2021/02/angular-11-bootstrap-4.5-Ecommerce-Template-Free.png' }];
+  //  //code ends here
+
+  //slider setting variable
+  responsiveOptions: any;
+
+  //define validable to store dynamic products data
+  products: any;
+
+  constructor(private http: HttpClient) {
+    //slider responsive settings
+    this.responsiveOptions = [
+      {
+        breakpoint: '1024px',
+        numVisible: 3,
+        numScroll: 3
+      },
+      {
+        breakpoint: '768px',
+        numVisible: 3,
+        numScroll: 2
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 3,
+        numScroll: 1
+      },
+      {
+        breakpoint: '414px',
+        numVisible: 3,
+        numScroll: 1
+      }
+    ];
+
+    //get request
+    this.http.get('https://www.testjsonapi.com/products/').subscribe(data => {
+      //data storing for use in html component
+      this.products = data;
+    }, error => console.error(error));
+  }
 
   ngOnInit(): void {
   }
 
 }
+
+
+
+
